5dc42d22367899653219f06171e92b582086ba73
samridhxshetty <samridhshetty2007@gmail.com>
2025-11-16 14:42:43 +0530

    Firebase Resetup v8

--- api/env.js
+++ api/env.js
@@ -1,1 +1,6 @@
-window.VITE_FIREBASE_API_KEY = AIzaSyDGpuBp7gt2fvMwLJJWLO8cqDE1egSoIKQ;
+export default function handler(req, res) {
+  res.setHeader("Content-Type", "application/javascript");
+  res.send(`
+    window.VITE_FIREBASE_API_KEY = "${process.env.VITE_FIREBASE_API_KEY}";
+  `);
+}
