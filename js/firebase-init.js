7eb5b0178078f33ce8d8e6f8f35187039fb78913
Samridh S Shetty <86785261+samxshetty@users.noreply.github.com>
2025-11-22 17:53:49 +0530

    Security Fix

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -4,13 +4,7 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-aut
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "ai-placement-portal-ca26c.firebaseapp.com",
-  projectId: "ai-placement-portal-ca26c",
-  storageBucket: "ai-placement-portal-ca26c.firebasestorage.app",
-  messagingSenderId: "857694851640",
-  appId: "1:857694851640:web:3211425de71a79d9840ccd",
-  measurementId: "G-3KYRZJD49S"
+
 };
 
 const app = initializeApp(firebaseConfig);
